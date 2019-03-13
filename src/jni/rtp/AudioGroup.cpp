66e4b1d1cce6d6ee2006e56af215bb20cb9ea820
Andy Hung <hunga@google.com>
2019-03-13 10:35:17 -0700

    AudioGroup: Do not specify AudioEffect io handle on client side
    The record sessionId should be sufficient. Besides the
    record could have moved to a different io handle in the
    interim.
    
    Test: compiles
    Bug: 128519136
    Change-Id: Ic5235acd447e7714314217a2fdfe481a1a01833d

--- src/jni/rtp/AudioGroup.cpp
+++ src/jni/rtp/AudioGroup.cpp
@@ -847,7 +847,7 @@ bool AudioGroup::DeviceThread::threadLoop()
                                     0,
                                     0,
                                     record->getSessionId(),
-                                    record->getInput());
+                                    AUDIO_IO_HANDLE_NONE); // record sessionId is sufficient.
             status_t status = aec->initCheck();
             if (status == NO_ERROR || status == ALREADY_EXISTS) {
                 aec->setEnabled(true);
